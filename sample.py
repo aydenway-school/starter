0b0bec7325044802c2e1531c1889569f4fa1b24e
Ayden Way <aydenway@uoregon.edu>
2025-09-30 10:39:56 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
-print("hello, world")
+import math
+
+print("hello, this is Ayden practicing for CS210")
+
+i = 0
+
+while i < 1000:
+    print(math.sqrt(i))
+    i += 1
+    if i % 2 == 0:
+        print(f"This number {i} is even")
+
